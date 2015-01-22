bdf602428457cb41f418cf7af4ce188b69fa9ba3
Suraj Narayanan <surajx@gmail.com>
2015-01-22 14:57:12 +0530

    Initial Commit

---
+++ ntfs-mount/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ntfs-mount
+//
+//  Created by Suraj Narayanan on 22/01/15.
+//  Copyright (c) 2015 Suraj Narayanan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
